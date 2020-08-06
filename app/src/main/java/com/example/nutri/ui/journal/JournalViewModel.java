e360d0d1818cbb7b41ee9ac657df57887f156d09
Sunil <luck_sunilthapa@yahoo.com>
2020-08-06 10:47:01 +1000

    file created

---
+++ app/src/main/java/com/example/nutri/ui/journal/JournalViewModel.java
@@ -0,0 +1,19 @@
+package com.example.nutri.ui.journal;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class JournalViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public JournalViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Journal fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
