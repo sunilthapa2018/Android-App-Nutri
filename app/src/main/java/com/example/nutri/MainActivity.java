0c73a58e35ae2eaacd79ebf2e7c71db168c843d0
Sunil <luck_sunilthapa@yahoo.com>
2020-08-06 11:04:31 +1000

    Commit from android studio

--- app/src/main/java/com/example/nutri/MainActivity.java
+++ app/src/main/java/com/example/nutri/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_search, R.id.navigation_journal, R.id.navigation_profile)
                 .build();
